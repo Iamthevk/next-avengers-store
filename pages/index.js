cbf110a3bb39cca8d25b36b0147e63c8989a79d2
veekay17 <vijay.vuex@gmail.com>
2022-08-07 14:38:24 +0530

    products json added

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,9 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-
+import Product from "../products.json";
 export default function Home() {
+  console.log(Product);
   return (
     <div className={styles.container}>
       <Head>
